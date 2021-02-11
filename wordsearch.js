82d6b3a7f8871d18f3c9460dc936b0a5106449c3
Eppa Lea Turniawan <eppalea@gmail.com>
2021-02-11 01:50:49 +0000

    modified the code to search for the word parameter vertically in the provided array. and if there is no array provided, to return false

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const verticalJoin = letters.map(vs => vs.join(''));
+  const verticalArray = [];
+  if (letters === []) {
+    return false;
+  }
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
-}
-
-module.exports = wordSearch
+  }
+  for (let i = 0; i < verticalJoin.length; i++) {
+    for (let j = 0; j < verticalJoin[i].length; j++) {
+      // console.log(verticalJoin[j][i]);
+      verticalArray.push(verticalJoin[j][i]);
+    }
+  }
+  // console.log(verticalArray);
+  // let almostFinal = verticalArray.join("").includes(word);
+  // console.log('does the word exist vert: ', almostFinal);
+  if (verticalArray.join("").includes(word)) {
+    return true;
+  }
+  return false;
+};
+    
+module.exports = wordSearch;
+    
